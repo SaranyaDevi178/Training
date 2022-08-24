5e0bd19e0b3103d13547156f89164610cf19eba5
unknown <cogdotnet1688@iiht.tech>
2022-08-24 18:35:24 +0530

    Initial commit

---
+++ Demo1/Demo1/Controllers/StudentsController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        public static  List<Students> stud = new List<Students>();
+
+        [HttpPost]
+        public IActionResult PostStudrecord(int id,string name, string department)
+        {
+            stud.Add(new Students
+            {
+                StudId = id,
+               StudName = name,
+                Department = department
+            });
+            return Ok("Added");
+        }
+
+        [HttpGet]
+        public IEnumerable<Students> Getdata()
+        {
+            return stud;
+        }
+        [HttpDelete]
+        public IActionResult delete(int id)
+        {
+            bool IsExist = stud.Contains(stud.Find(studs => studs.StudId.Equals(id)));
+            if (IsExist == true)
+            {
+                stud.Remove(stud.Find(studs => studs.StudId.Equals(id)));
+                return Ok("deleted");
+            }
+            else
+            {
+                return BadRequest("Request Not processed");
+            }
+        }
+
+        [HttpPut]
+        public IActionResult ModifingRecord(int id,  string name,string department)
+        {
+            bool IsExist = stud.Contains(stud.Find(studs => studs.StudId.Equals(id)));
+
+            try
+            {
+                if (IsExist==true)
+                {
+                   
+                    stud.Find (studs => studs.StudId.Equals(id)).StudName = name;
+
+                    return Ok("modified");
+                }
+                
+                else
+                {
+                    return BadRequest("Incorrect data");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return Ok("modified");
+           
+        }
+
+
+    }
+}
---
+++ Demo1/Demo1/Students.cs
@@ -0,0 +1,9 @@
+﻿namespace Demo1
+{
+    public class Students
+    {
+        public int StudId { get; set; }
+       public string? StudName { get; set; }
+        public String? Department { get; set; }
+    }
+}
